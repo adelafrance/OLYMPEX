3e8b04561b0b3daee3c5690de46968d2019294b3
Andrew DeLaFrance <adelaf@uw.edu>
2019-06-28 12:53:06 -0700

    Incorporate sounding to compare

--- comparebb2narr_west.py
+++ comparebb2narr_west.py
@@ -24,21 +24,25 @@ dir = 'west' #look east or west (lowercase)
 
 output_dir = '/home/disk/meso-home/adelaf/OLYMPEX/Output/BrightBands/' #location of previous output
 data_dir = '/home/disk/meso-home/adelaf/OLYMPEX/Data/' #directory for local data
-bb_data = ''.join(['brightbandsfound_v6_r_6_time0.25x15.0pcntx25.0_withrhohv_0.910.97_',dir,'.npy'])
+bb_data = ''.join(['brightbandsfound_v6_r_6_time0x35.0pcntx25.0_withrhohv_0.910.97_',dir,'.npy'])
+sounding_data = 'NPOL_soundings_melt_levs.npy'
 NARR_data = 'NARR_at_NPOL.csv'
-save_name_data_csv = ''.join(['BrightBandsXNARR_v6_r_6_time0.25x15.0pcntx25.0_withrhohv_0.910.97_',dir,'.csv'])
-save_name_fig = ''.join(['BrightBandsXNARR_v6_r_6_time0.25x15.0pcntx25.0_withrhohv_0.910.97_',dir,'.png'])
+save_name_data_csv = ''.join(['BrightBandsXNARRXNPOL_v6_r_6_time0x35.0pcntx25.0_withrhohv_0.910.97_',dir,'.csv'])
+save_name_fig = ''.join(['BrightBandsXNARRXNPOL_v6_r_6_time0x35.0pcntx25.0_withrhohv_0.910.97_',dir,'.png'])
 bb_fn = ''.join([output_dir,bb_data])
+sounding_fn = ''.join([output_dir,souding_data])
 NARR_fn = ''.join([data_dir,NARR_data])
 save_fn_data_csv = ''.join([output_dir,save_name_data_csv])
 save_fn_fig = ''.join([output_dir,save_name_fig])
 
 bright_bands = np.load(bb_fn)#time,bbfound, top, btm, bbcrit1, bbcrit2
+NPOL_data = np.load(souding_fn)
 df=pd.read_csv(NARR_fn, sep=',',header=None)
 NARR_data = np.array(df) #NARR Time,IVT,Melting Level (m),925speed (kt),925dir,Nd,Nm
 
 n_bbs = bright_bands.shape[0]
 n_NARRs = NARR_data.shape[0]
+n_NPOLs = NPOL_data.shape[0]
 
 #restrict plotting to only use times that a bright band occurred
 for i in range(0,n_bbs):
@@ -47,8 +51,9 @@ for i in range(0,n_bbs):
 
 BrightBands_w_NARR = bright_bands[:,[0,2]] #assign bright bands I found to the array
 NARR_melt_levs = bright_bands[:,[0,2]] #place holder for data just to build array, will be replaced later
+NPOL_melt_levs = bright_bands[:,[0,2]]
 #array structure = NPOL date, BB top found in algorithm, NARR date, melting layer
-BrightBands_w_NARR = np.hstack((BrightBands_w_NARR,NARR_melt_levs))
+BrightBands_w_NARR = np.hstack((BrightBands_w_NARR,NARR_melt_levs,NPOL_melt_levs))
 
 #NARR_dates = NARR_data[0:n_NARRs,0]
 items = []
@@ -73,25 +78,38 @@ for i in range(1,n_bbs):
     BrightBands_w_NARR[i,2] = d
     BrightBands_w_NARR[i,3] = float(NARR_data[min_index,2].replace(',',''))/1000 #assign the NARR melting layer to my array
 
+    timedeltas = []
+    for j in range(0,n_NPOLs):
+        timedeltas.append(np.abs(pivot-NPOL_data[j,0]))
+    min_index = timedeltas.index(np.min(timedeltas)) +1
+    d = datetime.datetime.strptime(NPOL_data[min_index,0], '%m/%d/%y %H:%M:%S')
+    melt_layer = NPOL_data[min_index,1]
+    BrightBands_w_NARR[i,4] = d
+    BrightBands_w_NARR[i,5] = float(NPOL_data[min_index,2])/1000 #assign the NPOL melting layer to my array
+
 BrightBands_w_NARR = BrightBands_w_NARR[1:BrightBands_w_NARR.shape[0],:] #just remove first row of index values
 BrightBands_w_NARR = BrightBands_w_NARR[BrightBands_w_NARR[:,0].argsort()]
 
 #save the data
 pd.DataFrame(BrightBands_w_NARR).to_csv(save_fn_data_csv)
 
-xdatesNPOL = np.empty(BrightBands_w_NARR.shape[0])
+xdatesBB = np.empty(BrightBands_w_NARR.shape[0])
 for xi in range(0,BrightBands_w_NARR.shape[0]):
-    xdatesNPOL[xi] = dates.date2num([datetime.datetime.strptime(BrightBands_w_NARR[xi,0], '%m/%d/%y %H:%M:%S')])
+    xdatesBB[xi] = dates.date2num([datetime.datetime.strptime(BrightBands_w_NARR[xi,0], '%m/%d/%y %H:%M:%S')])
 xdatesNARR = np.empty(BrightBands_w_NARR.shape[0])
 for xi in range(0,BrightBands_w_NARR.shape[0]):
     xdatesNARR[xi] = dates.date2num([datetime.datetime.strptime(BrightBands_w_NARR[xi,2], '%m/%d/%y %H:%M:%S')])
+xdatesNPOL = np.empty(BrightBands_w_NARR.shape[0])
+for xi in range(0,BrightBands_w_NARR.shape[0]):
+    xdatesNPOL[xi] = dates.date2num([datetime.datetime.strptime(BrightBands_w_NARR[xi,4], '%m/%d/%y %H:%M:%S')])
 
 #plot the data
 fig, ax = plt.subplots()
 days = dates.DayLocator(interval = 2)
 d_fmt = dates.DateFormatter('%m/%d/%y')
 ax.plot(xdatesNARR,BrightBands_w_NARR[:,3], label = 'NARR Melt Level', color = 'gray')
-ax.plot(xdatesNPOL,BrightBands_w_NARR[:,1], label = 'NPOL BB Level', color = "#1b9e77", linestyle = ':', linewidth = 1.5)
+ax.plot(xdatesBB,BrightBands_w_NARR[:,1], label = 'NPOL Radar Level', color = "#1b9e77", linestyle = ':', linewidth = 1.5)
+ax.plot(xdatesNPOL,BrightBands_w_NARR[:,5], label = 'NPOL Sounding Level', color = "blue", linestyle = ':', linewidth = 1.5)
 #ax.xticks(xdatesNPOL,BrightBands_w_NARR[:,0])
 ax.xaxis.set_major_locator(days)
 ax.xaxis.set_major_formatter(d_fmt)
